63cbffa841cafecc26b0edc99156b8ec927ee80e
Danya <danissimo02032007@gmail.com>
2025-12-07 23:15:07 +0300

    delete cout in client.cpp

--- client.cpp
+++ client.cpp
@@ -42,7 +42,6 @@ void client::do_connect(const tcp::resolver::results_type& endpoints) {
                 self->target_.ping_milliseconds = self->time.get_ms();
                 self->target_.ping_microseconds = self->time.get_mcs();
                 self->target_.addr = self->endpoint_.address().to_string();
-                std::cout << endpoint.address().to_string() << ":" << endpoint.port() << " time to conn: " << self->target_.ping_milliseconds.count() << "ms." << std::endl;
                 self->c_.setPing(self->target_);
             } else {
                 std::cerr << "Error: " << ec.message() << std::endl;
