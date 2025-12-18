2ee4ffb80170e7a25a68ba1783f924452341a20a
DanMenshi <157708929+DanMenshi@users.noreply.github.com>
2025-12-18 14:27:47 +0300

    edited main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
 #include "config.h"
 #include "client.h"
-
+#include "app.h"
 int main() {
     boost::asio::io_context io;
     config s{};
+    App a(io, s);
+    a.choice();
 
-    for (auto t : s.getTargets()) {
-        std::make_shared<client>(io, t, s)->start();
-    }
-
-
-    io.run();
     return 1;
 }
